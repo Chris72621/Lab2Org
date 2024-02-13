2f5831974baad5c15892af3e8cbfb7a464477e7e
Christian Rodriguez <christianrodriguez@ChristiansAir7.lan>
2024-02-12 19:36:16 -0700

    added new files and doing methods

--- my_string.c
+++ my_string.c
@@ -50,7 +50,7 @@ int str_n_cmp(const char *s1, const char *s2, size_t n) {
         int s2_char_val = (int) *s2;
 
         if (s1_char_val < s2_char_val) {
-            return s1_char_val - s2_char_val; // return diffrence=
+            return s1_char_val - s2_char_val; // return diffrence
         } else if (s1_char_val > s2_char_val) {
             return s1_char_val - s2_char_val; // return diffrence
         }
@@ -72,17 +72,78 @@ int get_largest_length(const char *s1, const char *s2) {
     return length_s2;
 }
 
+/*
+    locates the first occurrence of c (converted to a char) in the string pointed to by s.
+    return a pointer to the located character, or NULL if the character does not appear in the string.
+*/
+char * str_chr(const char *s, int c){
+    while (*s != '\0') {
+        if (*s == (char) c) {
+            return (char *)s;
+        }
+        s++;
+    }
+
+    return NULL;
+}
+
+
+/*
+    locates in the null-terminated string s the first occurrence of any character in the string charset and returns a pointer to this character.
+    If no characters from charset occur anywhere in s strpbrk() returns NULL.
+*/
+char * str_pbrk(const char *s, const char *charset) {
+    const char *resetCharSet = charset;
+
+    while (*s != '\0') {
+        charset = resetCharSet;
+
+        while (*charset != '\0') {
+            if (*charset == *s) {
+                return (char *)s;
+            }
+            charset++;
+        }
+        s++;
+    }
+
+    return NULL;
+}
+
+/*
+    finds the first occurrence of any character from a set of characters in a given string. 
+    It replaces that character with a null terminator and returns the part of the string before the delimiter. 
+    If the string is empty or the end is reached, it returns NULL.
+*/
+char * str_sep(char **stringp, const char *delim) {
+
+    return NULL;
+}
+
+
+/*
+    function appends not more than n characters from s2, and then adds a terminating ‘\0’.
+    return the pointer s1.
+*/
+char * str_cat(char *s1, const char *s2) {
+    int length = str_len(s1) + str_len(s2);
+    char newString [length];
+
+    
+    
+}
+
+
 int main () {
 
     const char *s1 = "Hello";
-    const char *s2 =  "ye";
+    const char *s2 =  "aaaae";
 
-    int test = str_n_cmp(s1,s2,0);
-    int actual = strncmp(s1,s2,0);
+    char *test = str_pbrk(s1,s2);
+    char *actual = strpbrk(s1,s2);
 
-    printf("Value of 'test': %d\n", test);
-    printf("Value of 'actual': %d\n", actual);
-
+    printf("Test result: %p\n", (void *)test);
+    printf("Actual result: %p\n", (void *)actual);
 
     return 0;
 }
